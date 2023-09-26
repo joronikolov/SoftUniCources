909fb2ecd13b2f008b7556a027e787976050cee1
Joro Nikolov <uninportantposhta@gmail.com>
2023-09-26 22:47:50 +0300

    Revert "25 percent working"
    This reverts commit 657b6510390c4e38d05aa8573984926aa552d05d.

--- ProgrammingFundamentals/MoreExerciseDataTypesAndVariables/src/FromLeftToTheRight.java
+++ ProgrammingFundamentals/MoreExerciseDataTypesAndVariables/src/FromLeftToTheRight.java
@@ -1,9 +1,4 @@
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
 import java.util.Scanner;
-
-import static java.math.RoundingMode.*;
 
 public class FromLeftToTheRight {
     public static void main(String[] args) {
@@ -13,26 +8,22 @@ public class FromLeftToTheRight {
             String currentString = scan.nextLine();
             String string1 = currentString.split(" ")[0];
             String string2 = currentString.split(" ")[1];
-//            long left = Long.parseLong(string1);
-//            long right = Long.parseLong(string2);
-            BigDecimal left = new BigDecimal(string1);
-            BigDecimal right = new BigDecimal(string2);
-            BigDecimal sum = BigDecimal.valueOf(0);
-            if (left.compareTo(right) > 0) {
-                while (left.compareTo(BigDecimal.valueOf(0)) > 0) {
-                    sum = sum.add(left.remainder(BigDecimal.valueOf(10)));
-                    left = left.divide(BigDecimal.valueOf(10)).round(new MathContext(1, RoundingMode.FLOOR));
+            long left = Long.parseLong(string1);
+            long right = Long.parseLong(string2);
+            long sum = 0;
+            if (left > right) {
+                while (left > 0) {
+                    sum += left % 10;
+                    left /= 10;
 
                 }
             } else {
-                while (right.compareTo(BigDecimal.valueOf(0)) > 0) {
-                    sum = sum.add(right.remainder(BigDecimal.valueOf(10)));
-
-                    right = right.divide(BigDecimal.valueOf(10)).round(new MathContext(1, RoundingMode.FLOOR)) ;
-
+                while (right > 0) {
+                    sum += right % 10;
+                    right /= 10;
                 }
-                System.out.println(sum);
             }
+            System.out.println(sum);
         }
 
     }
